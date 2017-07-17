2fdb48362cfcda0a66d7332b858647044d2221eb
mohammad <shalabi67@hotmail.com>
2017-07-18 01:05:30 +0200

    adding aggregate and fixing project structure

---
+++ Java8/Streams/lib/src/main/java/com/streams/Trader.java
@@ -0,0 +1,22 @@
+package com.streams;
+
+/**
+ * Created by mohammad on 11/30/2016.
+ */
+public class Trader{
+    private final String name;
+    private final String city;
+    public Trader(String n, String c){
+        this.name = n;
+        this.city = c;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public String getCity(){
+        return this.city;
+    }
+    public String toString(){
+        return "Trader:"+this.name + " in " + this.city;
+    }
+}
