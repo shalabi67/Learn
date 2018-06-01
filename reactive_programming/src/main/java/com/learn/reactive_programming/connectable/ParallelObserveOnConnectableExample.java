e487805fbafdecd25dea020ceb012bc0116a294e
mohammad <shalabi67@hotmail.com>
2018-06-01 02:50:11 +0200

    adding connectable

---
+++ reactive_programming/src/main/java/com/learn/reactive_programming/connectable/ParallelObserveOnConnectableExample.java
@@ -0,0 +1,61 @@
+package com.learn.reactive_programming.connectable;
+
+
+import com.learn.reactive_programming.util.ThreadUtils;
+import com.learn.reactive_programming.util.TimeTicker;
+import io.reactivex.observables.ConnectableObservable;
+import io.reactivex.schedulers.Schedulers;
+
+public class ParallelObserveOnConnectableExample {
+
+    public static void main(String[] args) {
+
+        // Create a ticker that goes off every 1/2 second.
+        TimeTicker ticker = new TimeTicker(500);
+        ticker.start();
+        
+        // From the ticker, we create a connectable observable by 
+        // get the observable and then calling its "publish" method.
+        ConnectableObservable<Long> connectable = ticker
+                .toObservable()
+                .publish();
+
+        // Next we do our subscription....
+        connectable
+                // We want to run this on a different thread than the ticker thread
+                .observeOn(Schedulers.computation())
+                .subscribe(
+                        (t) -> {
+                            System.out.println("Tick: " + ThreadUtils.currentThreadName() + " " + t);
+                        }
+                );
+        
+        connectable
+                // We want to run this on a different thread than the ticker thread
+                .observeOn(Schedulers.computation())
+                .subscribe(
+                        (t) -> {
+                            System.out.println("Tick2: " + ThreadUtils.currentThreadName() + " " + t);
+                        }
+                );
+
+        // But notice how for 3 seconds nothing happens until we call "connect"
+        System.out.println("Sleeping for 3 seconds...");
+        ThreadUtils.sleep(3000);
+
+        // Now we call "connect" on the connectable observable...and things start happening...
+        System.out.println("Connecting...");
+        connectable.connect();
+
+        // Let the ticker do its thing for 3 seconds so we can see events
+        // flowing...
+        // Notice that we are now getting parallel behavior between the observer
+        // threads.
+        ThreadUtils.sleep(3000);
+        System.out.println("Three seconds are up!");
+        
+        // Stop the ticker and kill the example's VM
+        ticker.stop();
+        System.exit(0);
+    }
+}
